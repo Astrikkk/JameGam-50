75194f04bff9df6a85930a01be3393c75972c55a
Nydaxe <james.salehpour@gmail.com>
2025-06-15 16:30:29 -0400

    Fixed camelcase

--- Project M.O.B.Y/Assets/Scripts/GamePlay/Equipment.cs
+++ Project M.O.B.Y/Assets/Scripts/GamePlay/Equipment.cs
@@ -6,11 +6,11 @@ public class Equipment : MonoBehaviour
 {
     [SerializeField] private bool isWorking = true;
     [SerializeField] private bool isBroken = true;
-    [SerializeField] private GameObject AlertSign; //Appear on top of broken equipment to show that it is broken, get atention
-    [SerializeField] private GameObject FixMenu;
-    [SerializeField] private GameObject ActionMenu;
+    [SerializeField] private GameObject alertSign; //Appear on top of broken equipment to show that it is broken, get atention
+    [SerializeField] private GameObject fixMenu;
+    [SerializeField] private GameObject actionMenu;
 
-    [SerializeField] private bool ActivePlayer = false;
+    [SerializeField] private bool activePlayer = false;
 
 
     
@@ -22,10 +22,10 @@ public class Equipment : MonoBehaviour
         isBroken = true;
     }
 
-    private void ActionActivation(bool State) //will activate such things as menu, anumation and so on if needeed
+    private void ActionActivation(bool state) //will activate such things as menu, anumation and so on if needeed
     {
-        ActivePlayer = State;
-        ActionMenu.SetActive(State);
+        activePlayer = state;
+        actionMenu.SetActive(state);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
